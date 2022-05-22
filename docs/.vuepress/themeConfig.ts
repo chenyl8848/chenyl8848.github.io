4ebc6e554f3aadbdd7580e150e0c29399a6c2f83
cyl <1063415880@qq.com>
2022-05-22 22:32:55 +0800

    添加搜索插件

--- docs/.vuepress/themeConfig.ts
+++ docs/.vuepress/themeConfig.ts
@@ -111,54 +111,57 @@ export default defineThemeConfig({
       // serverURL: "https://vuepress-theme-hope-comment.vercel.app",
     },
 
-    // docsearch: {
-    //   appId: "LDBQGQC8Q9",
-    //   apiKey: "5c3a7145aeba231c3b85b742d24fc24f",
-    //   indexName: "mrhope",
-    //   locales: {
-    //     "/": {
-    //       placeholder: "搜索",
-    //       translations: {
-    //         button: {
-    //           buttonText: "搜索",
-    //           buttonAriaLabel: "搜索",
-    //         },
-    //         modal: {
-    //           searchBox: {
-    //             resetButtonTitle: "清除查询条件",
-    //             resetButtonAriaLabel: "清除查询条件",
-    //             cancelButtonText: "取消",
-    //             cancelButtonAriaLabel: "取消",
-    //           },
-    //           startScreen: {
-    //             recentSearchesTitle: "搜索历史",
-    //             noRecentSearchesText: "没有搜索历史",
-    //             saveRecentSearchButtonTitle: "保存至搜索历史",
-    //             removeRecentSearchButtonTitle: "从搜索历史中移除",
-    //             favoriteSearchesTitle: "收藏",
-    //             removeFavoriteSearchButtonTitle: "从收藏中移除",
-    //           },
-    //           errorScreen: {
-    //             titleText: "无法获取结果",
-    //             helpText: "你可能需要检查你的网络连接",
-    //           },
-    //           footer: {
-    //             selectText: "选择",
-    //             navigateText: "切换",
-    //             closeText: "关闭",
-    //             searchByText: "搜索提供者",
-    //           },
-    //           noResultsScreen: {
-    //             noResultsText: "无法找到相关结果",
-    //             suggestedQueryText: "你可以尝试查询",
-    //             openIssueText: "你认为该查询应该有结果？",
-    //             openIssueLinkText: "点击反馈",
-    //           },
-    //         },
-    //       },
-    //     },
-    //   },
-    // },
+    docsearch: {
+      appId: "ZVX0G03WWG",
+      apiKey: "8261872ff25256de3430991647d07137",
+      indexName: "chenyl8848",
+      // appId: "LDBQGQC8Q9",
+      // apiKey: "5c3a7145aeba231c3b85b742d24fc24f",
+      // indexName: "mrhope",
+      locales: {
+        "/": {
+          placeholder: "搜索",
+          translations: {
+            button: {
+              buttonText: "搜索",
+              buttonAriaLabel: "搜索",
+            },
+            modal: {
+              searchBox: {
+                resetButtonTitle: "清除查询条件",
+                resetButtonAriaLabel: "清除查询条件",
+                cancelButtonText: "取消",
+                cancelButtonAriaLabel: "取消",
+              },
+              startScreen: {
+                recentSearchesTitle: "搜索历史",
+                noRecentSearchesText: "没有搜索历史",
+                saveRecentSearchButtonTitle: "保存至搜索历史",
+                removeRecentSearchButtonTitle: "从搜索历史中移除",
+                favoriteSearchesTitle: "收藏",
+                removeFavoriteSearchButtonTitle: "从收藏中移除",
+              },
+              errorScreen: {
+                titleText: "无法获取结果",
+                helpText: "你可能需要检查你的网络连接",
+              },
+              footer: {
+                selectText: "选择",
+                navigateText: "切换",
+                closeText: "关闭",
+                searchByText: "搜索提供者",
+              },
+              noResultsScreen: {
+                noResultsText: "无法找到相关结果",
+                suggestedQueryText: "你可以尝试查询",
+                openIssueText: "你认为该查询应该有结果？",
+                openIssueLinkText: "点击反馈",
+              },
+            },
+          },
+        },
+      },
+    },
 
     mdEnhance: {
       enableAll: true,
