0d830c86874433e3fa8602b55ff669ad40dad48f
chenyl8848 <1063415880@qq.com>
2024-10-26 22:21:15 +0800

    feat:添加Spring6

--- docs/.vuepress/sidebar/rearend/spring.ts
+++ docs/.vuepress/sidebar/rearend/spring.ts
@@ -2,6 +2,7 @@ import type { HopeThemeSidebarArrayConfig } from "vuepress-theme-hope";
 
 export const spring: HopeThemeSidebarArrayConfig = [
     "",
+    "Spring6",
     "Spring_DI",
     "Spring_Retry",
     "SpringSecurity+Oauth2"
--- docs/rearend/spring/README.md
+++ docs/rearend/spring/README.md
@@ -6,6 +6,7 @@ isOriginal: true
 article: false
 ---
 
+- [Spring6 万字入门教程](Spring6.md)
 - [Spring 注入方式](Spring_DI.md)
 - [Spring Retry 实现重试机制](Spring_Retry.md)
 - [SpringSecurity + OAuth2 权限管理实战](SpringSecurity+Oauth2.md)
